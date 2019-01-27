e81ac4492f763531722381c19e914e3990688947
Abhishek Aryan <send2abhishek@live.com>
2019-01-27 23:51:48 +0530

    Add project files.

---
+++ BloodTraceWebApi/Controllers/BloodUsersController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BloodTraceWebApi.Helpers;
+using BloodTraceWebApi.Models;
+
+namespace BloodTraceWebApi.Controllers
+{
+    public class BloodUsersController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public IHttpActionResult Post(BloodUser bloodUser)
+        {
+
+            if (!ModelState.IsValid)
+            {
+
+                return BadRequest(ModelState);
+            }
+            //now we have to handle Images
+
+            var stream = new MemoryStream(bloodUser.ImageArray);
+
+            //Guid used for uniquely identify the resource
+            var guid = Guid.NewGuid().ToString();
+            var file = string.Format("{0}.jpg", guid);
+            var folder = "~/Content/Users";
+            var fullPath = string.Format("{0}/{1}", folder, file);
+
+            var response = FilesHelper.UploadPhoto(stream, folder, file);
+
+            if (response)
+            {
+                bloodUser.Imagepath = fullPath;
+            }
+
+            var user = new BloodUser()
+            {
+               UserName = bloodUser.UserName,
+                BloodGroup = bloodUser.BloodGroup,
+                Email=bloodUser.Email,
+                Phone=bloodUser.Phone,
+                Country=bloodUser.Country,
+                Date=bloodUser.Date,
+                Imagepath=bloodUser.Imagepath
+            };
+
+            db.BloodUsers.Add(user);
+            db.SaveChanges();
+            return StatusCode(HttpStatusCode.Created);
+        }
+
+        public IEnumerable<BloodUser> Get(string bloodGroup,string country)
+        {
+
+
+            var bloodUsers=db.BloodUsers.Where(user => user.BloodGroup.StartsWith(bloodGroup) && user.Country.StartsWith(country));
+            return bloodUsers;
+        }
+
+        public IEnumerable<BloodUser> Get()
+        {
+
+            //returns the latest USers
+            var latesUsers = db.BloodUsers.OrderByDescending(user => user.Date);
+            return latesUsers;
+        }
+
+
+    }
+}
---
+++ BloodTraceWebApi/Models/BloodUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BloodTraceWebApi.Models
+{
+    public class BloodUser
+    {
+
+        public int Id { get; set; }
+
+        [Required, StringLength(10)]
+        public string UserName { get; set; }
+
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email in not valid")]
+        public string Email { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid Phone Number")]
+        public string Phone { get; set; }
+
+        
+        public string Country { get; set; }
+
+        [Required, StringLength(2)]
+        public string BloodGroup { get; set; }
+        public string Imagepath { get; set; }
+
+        public int Date { get; set; }
+
+
+        //holds byte of array
+        //we don't want the column of ImageArray in our database and 
+        //that's why I am using NotMapped Attribute on top of this property
+
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
+    }
+}
